c05e0902b7a8bc8f8fa9ed531e07741bed662e3a
Alessandro Cervantes <77180915+dAlessandroH@users.noreply.github.com>
2025-11-16 22:31:36 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='simv3-ui-light-v1';
+const CACHE='simv3-ui-light-v2';
 const ASSETS=['./','./index.html','./css/style.css','./js/app.js','./manifest.json','./assets/icon-192.png','./assets/icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))))});
